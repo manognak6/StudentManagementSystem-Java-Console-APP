a256b9d043b0e278b471c99078e1583b6184dbb9
manognak6 <110725358+manognak6@users.noreply.github.com>
2025-07-28 14:47:06 +0530

    Update Main.java

--- src/com/studentdetailsapp/Main.java
+++ src/com/studentdetailsapp/Main.java
@@ -4,19 +4,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("**********Student Management System**********");
-		System.out.println("**********Welcome****************************");
-		Student s1;//s1 is a reference variable which stores unique identification hash code of the object stored in heap  whenever an object is created 3 things happen, those are 1. class is loaded into heap memory 2.instance variables are initialised 3constructor is called(if there are no parameters, default constructor is called.
-		s1= new Student("Manogna",28,"S-564");
-		s1.enrollCourse("Java");
-		s1.printStudentInfo();
-	//	Student s2= new Student("Leela", 24,"S-767");
-	//	s2.printStudentInfo();
-	//	s2.enrollCourse("C");
-		Student s3= new Student("Rohit",30,"S-7865");
-		s3.enrollCourse("Angular");
-		s3.enrollCourse(".Net Core");
-		s3.printStudentInfo();
-
-	}
-
-}
+//		System.out.println("**********Welcome****************************");
+//		//S1 IS A REFERENCE VARIABLE WHICH STORES UNIQUE IDENTIFICATION HASH CODE OF THE OBJECT STORED IN HEAP  WHENEVER AN OBJECT IS CREATED 3 THINGS HAPPEN, THOSE ARE 1. CLASS IS LOADED INTO HEAP MEMORY 2.INSTANCE VARIABLES ARE INITIALISED 3CONSTRUCTOR IS CALLED(IF THERE ARE NO PARAMETERS, DEFAULT CONSTRUCTOR IS CALLED.
+//		
+//		List<Student> studentlist=new ArrayList<Student>();
+//		Student s1 =new Student("manogna",28,"S-56");
+//		
+//		
+//		Scanner scanner= new Scanner(System.in);
+//		System.out.println("Enter Student name");
+//		String name=scanner.next();
+//		System.out.println("You entered name "+name);
+//		
+//		System.out.println("Enter student age");
+//		int age=scanner.nextInt();
+//		System.out.println("Student age you entered is "+age);
+//		
+//		System.out.println("Enter student ID");
+//		String studentId=scanner.next();
+//		System.out.println("Student ID you entered is "+studentId);
+//		
+//		System.out.println("Student Id is " +studentId);
+//		Student result = findStudentById("S-56");
+//		System.out.println("Result is "+result);
+//	}		
+//	
+//		public static Student findStudentById(String studentId) {
+//			Student result = null;
+//			try {
+//				result = studentlist.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId)).findFirst()
+//						.orElseThrow(() -> new RuntimeException("No Data found"));
+//
+//		}
+//			catch (RuntimeException e) {
+//				System.err.println("Student with ID "+studentId +" not found");
+//			}
+//			return result;
+//	}
+//
+//}
